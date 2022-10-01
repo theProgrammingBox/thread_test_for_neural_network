50b26e4b27d9b8b5d59cd306e1d26d4945ebb019
theProgrammingBox <justinboxgea@gmail.com>
2022-09-30 21:12:35 -0700

    reimplement inputs and output

--- thread_test/Randoms.h
+++ thread_test/Randoms.h
@@ -1,113 +1,117 @@
 #pragma once
 #include <chrono>
-#include <math.h>
 
-using namespace std::chrono;
+using std::chrono::duration_cast;
+using std::chrono::nanoseconds;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
 
 class Random
 {
-private:
-	uint64_t s[4];
+public:
+	Random(uint32_t seed = 0xc5004e441c522fb3) {
+		double dn = 3.442619855899;
+		int i;
+		const double m1 = 2147483648.0;
+		double q;
+		double tn = 3.442619855899;
+		const double vn = 9.91256303526217E-03;
 
-	uint64_t rotl(const uint64_t x, int k)
-	{
-		return (x << k) | (x >> (64 - k));
+		q = vn / exp(-0.5 * dn * dn);
+
+		kn[0] = (uint32_t)((dn / q) * m1);
+		kn[1] = 0;
+
+		wn[0] = (float)(q / m1);
+		wn[127] = (float)(dn / m1);
+
+		fn[0] = 1.0;
+		fn[127] = (float)(exp(-0.5 * dn * dn));
+
+		for (i = 126; 1 <= i; i--)
+		{
+			dn = sqrt(-2.0 * log(vn / dn + exp(-0.5 * dn * dn)));
+			kn[i + 1] = (uint32_t)((dn / tn) * m1);
+			tn = dn;
+			fn[i] = (float)(exp(-0.5 * dn * dn));
+			wn[i] = (float)(dn / m1);
+		}
+
+		this->seed = seed ^
+			duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count() ^
+			duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
 	}
 
-	void jump(void) {
-		static const uint64_t JUMP[] = { 0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c };
+	uint32_t uintRand()
+	{
+		uint32_t temp;
 
-		uint64_t s0 = 0;
-		uint64_t s1 = 0;
-		uint64_t s2 = 0;
-		uint64_t s3 = 0;
-		for (int i = 0; i < sizeof JUMP / sizeof * JUMP; i++)
-			for (int b = 0; b < 8; b++) {
-				if (JUMP[i] & UINT64_C(1) << b) {
-					s0 ^= s[0];
-					s1 ^= s[1];
-					s2 ^= s[2];
-					s3 ^= s[3];
-				}
-				ULongRandom();
+		temp = seed;
+		seed = (seed ^ (seed << 13));
+		seed = (seed ^ (seed >> 17));
+		seed = (seed ^ (seed << 5));
+
+		return temp + seed;
+	}
+
+	float floatRand()
+	{
+		uint32_t temp;
+
+		temp = seed;
+		seed = (seed ^ (seed << 13));
+		seed = (seed ^ (seed >> 17));
+		seed = (seed ^ (seed << 5));
+
+		return fmod(0.5 + (float)(temp + seed) * 2.32830643654e-10, 1.0);
+	}
+
+	float normalRand()
+	{
+		int hz;
+		uint32_t iz;
+		const float r = 3.442620;
+		float value;
+		float x;
+		float y;
+
+		hz = (int)uintRand();
+		iz = (hz & 127);
+
+		if (fabs(hz) < kn[iz]) {
+			return (float)(hz)*wn[iz];
+		}
+
+		while (true) {
+			if (iz == 0) {
+				do {
+					x = -0.2904764 * log(floatRand());
+					y = -log(floatRand());
+				} while (x * x > y + y);
+
+				return (hz <= 0) * (-r - x) + (hz > 0) * (r + x);
 			}
 
-		s[0] = s0;
-		s[1] = s1;
-		s[2] = s2;
-		s[3] = s3;
+			x = (float)(hz)*wn[iz];
+
+			if (fn[iz] + floatRand() * (fn[iz - 1] - fn[iz]) < exp(-0.5 * x * x))
+			{
+				return x;
+			}
+
+			hz = (int)uintRand();
+			iz = (hz & 127);
+
+			if (fabs(hz) < kn[iz])
+			{
+				return (float)(hz)*wn[iz];
+			}
+		}
 	}
 
-public:
-	Random(
-		uint64_t seed1 = duration_cast<seconds>(high_resolution_clock::now().time_since_epoch()).count(),
-		uint64_t seed2 = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count(),
-		uint64_t seed3 = duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count(),
-		uint64_t seed4 = duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count())
-	{
-		s[0] = 0x76e15d3efefdcbbf ^ seed1;
-		s[1] = 0xc5004e441c522fb3 ^ seed2;
-		s[2] = 0x77710069854ee241 ^ seed3;
-		s[3] = 0x39109bb02acbe635 ^ seed4;
-
-		jump();
-	}
-
-	void Seed(
-		uint64_t seed1 = duration_cast<seconds>(high_resolution_clock::now().time_since_epoch()).count(),
-		uint64_t seed2 = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count(),
-		uint64_t seed3 = duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count(),
-		uint64_t seed4 = duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count())
-	{
-		s[0] = 0x76e15d3efefdcbbf ^ seed1;
-		s[1] = 0xc5004e441c522fb3 ^ seed2;
-		s[2] = 0x77710069854ee241 ^ seed3;
-		s[3] = 0x39109bb02acbe635 ^ seed4;
-
-		jump();
-	}
-
-	uint64_t ULongRandom()
-	{
-		const uint64_t result = rotl(s[0] + s[3], 23) + s[0];
-		const uint64_t t = s[1] << 17;
-
-		s[2] ^= s[0];
-		s[3] ^= s[1];
-		s[1] ^= s[2];
-		s[0] ^= s[3];
-		s[2] ^= t;
-		s[3] = rotl(s[3], 45);
-
-		return result;
-	}
-
-	int64_t LongRandom()
-	{
-		return int64_t(ULongRandom());
-	}
-
-	double UDoubleRandom()	// 0 through 1
-	{
-		return ULongRandom() * 5.42101086243e-20;
-	}
-
-	double DoubleRandom()	// -1 through 1
-	{
-		return int64_t(ULongRandom()) * 1.08420217249e-19;
-	}
-
-	double NormalRandom(double mean = 0.0, double standardDeviation = 1.0)
-	{
-		double x, y, radius;
-		do
-		{
-			x = DoubleRandom();
-			y = DoubleRandom();
-
-			radius = x * x + y * y;
-		} while (radius >= 1.0 || radius == 0.0);
-
-		return x * sqrt(-2.0 * log(radius) / radius) * standardDeviation + mean;
-	}
+private:
+	uint32_t seed;
+	uint32_t kn[128];
+	float fn[128];
+	float wn[128];
 };
--- thread_test/Source1.cpp
+++ thread_test/Source1.cpp
@@ -7,208 +7,198 @@ using std::cout;
 using std::endl;
 using std::swap;
 
-void initParams(float* initialInputs, float* weights, float* biases, uint64_t numNodes, uint64_t row, uint16_t numRows) {
-	Random r(row, numRows);
-	for (uint64_t i = 0; i < numRows; i++) {
-		initialInputs[row + i] = r.DoubleRandom();
+void initParams(float* initialInputs, float* weights, float* biases, uint64_t numNodes, uint64_t numInputs, uint64_t currentNode, uint16_t nodes) {
+	Random r;
+	for (uint64_t i = currentNode; i < currentNode + nodes; i++) {
+		initialInputs[i * (i < numNodes)] = r.normalRand() * 0.1f;
 		for (uint64_t j = 0; j < numNodes; j++) {
-			weights[(row + i) * numNodes + j] = (row + i == j) + r.DoubleRandom() * 0.1;
+			weights[i * numInputs + j] = (i == j) + r.normalRand() * 0.1f;
 		}
-		biases[row + i] = r.DoubleRandom() * 0.1;
+		for (uint64_t j = numNodes; j < numInputs; j++) {
+			weights[i * numInputs + j] = r.normalRand() * 0.1f;
+		}
+		biases[i] = r.normalRand() * 0.1f;
 	}
 }
 
-void multiplyWeightsAndBiases(float* weights, float* biases, float* inputs, float* outputs, float* perseptions, float* actions, uint64_t numNodes, uint64_t row, uint16_t numRows) {
-	for (uint64_t i = 0; i < numRows; i++) {
-		outputs[row + i] = biases[row + i];
-		for (uint64_t j = 0; j < numNodes; j++) {
-			outputs[row + i] += weights[(row + i) * numNodes + j] * inputs[j];
-		}
-		outputs[row + i] *= (outputs[row + i] < 0.0f) * 1.1f - 0.1f;
-	}
-}
+//void multiplyWeightsAndBiases(float* weights, float* biases, float* inputs, float* outputs, float* perseptions, float* actions, uint64_t numNodes, uint64_t currentOutput, uint16_t numOutputs) {
+//	for (uint64_t i = 0; i < numOutputs; i++) {
+//		outputs[currentOutput + i] = biases[currentOutput + i];
+//		for (uint64_t j = 0; j < numNodes; j++) {
+//			outputs[currentOutput + i] += weights[(currentOutput + i) * numNodes + j] * inputs[j];
+//		}
+//		outputs[currentOutput + i] *= (outputs[currentOutput + i] < 0.0f) * 1.1f - 0.1f;
+//	}
+//}
 
-class Network {
+class Network {						// only input and output layer, think of it like a brain's state of mind changing over time
 public:
-	uint64_t numThreads;
-	uint64_t numNodesPerThread;
-	uint64_t numRemainingNodes;
-	uint64_t numNodes;
-	uint64_t numWeights;
-	uint64_t numPerseptions;
-	uint64_t numActions;
-	thread* threads;
-	float* initialInputs;
-	float* weights;
-	float* biases;
-	float* inputs;
-	float* outputs;
-	float* perceptions;
-	float* actions;
+	uint64_t numNodes;				// number of purely reccursive nodes in the network, the "brain"
+	uint64_t numPerseptions;		// number of inputs to the network, the "senses"
+	uint64_t numActions;			// number of outputs from the network, the "actions"
+	uint64_t numInputs;				// number of inputs to the network, contains the perceptions and the outputs or "brain" of the previous iteration
+	uint64_t numOutputs;			// number of outputs from the network, contains the actions and the outputs or changed "brain" from the current iteration
+	uint64_t numWeights;			// number of weights in the network
+	float* inputs;					// inputs to the network, contains the perceptions and the outputs or "brain" of the previous iteration
+	float* outputs;					// outputs from the network, contains the actions and the outputs or changed "brain" from the current iteration
+	float* initialInputs;			// initial state of mind during "birth"
+	float* weights;					// weights of the network
+	float* biases;					// biases of the network
 
-	Network(uint64_t numNodes = 1000, uint64_t numThreads = 8, uint64_t numPerseptions = 2, uint64_t numActions = 1) {
-		this->numThreads = numThreads;
-		this->numNodesPerThread = numNodes / numThreads;
-		this->numRemainingNodes = numNodes - numNodesPerThread * numThreads;
+	uint64_t numThreads;			// for parallelization
+	uint64_t numNodesPerThread;		// minimum number of nodes handled per thread
+	uint64_t numRemainingNodes;		// number of threads handling one more node
+	thread* threads;				// array of threads
+
+	Network(uint64_t numNodes = 1000, uint64_t numPerseptions = 2, uint64_t numActions = 1, uint64_t numThreads = 4) {
 		this->numNodes = numNodes;
-		this->numWeights = numNodes * numNodes;
 		this->numPerseptions = numPerseptions;
 		this->numActions = numActions;
-		this->threads = new thread[numThreads];
+		this->numInputs = numPerseptions + numNodes;
+		this->numOutputs = numActions + numNodes;
+		this->numWeights = numInputs * numOutputs;
+		this->inputs = new float[numInputs];
+		this->outputs = new float[numOutputs];
 		this->initialInputs = new float[numNodes];
 		this->weights = new float[numWeights];
-		this->biases = new float[numNodes];
-		this->inputs = new float[numNodes];
-		this->outputs = new float[numNodes];
-		this->perceptions = new float[numPerseptions];
-		this->actions = new float[numActions];
+		this->biases = new float[numOutputs];
+		this->numThreads = numThreads;
+		this->numNodesPerThread = numOutputs / numThreads;
+		this->numRemainingNodes = numOutputs - numNodesPerThread * numThreads;
+		this->threads = new thread[numThreads];
 	}
 
 	~Network() {
-		delete[] threads;
+		delete[] inputs;
+		delete[] outputs;
 		delete[] initialInputs;
 		delete[] weights;
 		delete[] biases;
-		delete[] inputs;
-		delete[] outputs;
-		delete[] perceptions;
-		delete[] actions;
+		delete[] threads;
 	}
 
 	Network(const Network& other) {
-		this->numThreads = other.numThreads;
-		this->numNodesPerThread = other.numNodesPerThread;
-		this->numRemainingNodes = other.numRemainingNodes;
-		this->numNodes = other.numNodes;
-		this->numWeights = other.numWeights;
-		this->numPerseptions = other.numPerseptions;
-		this->numActions = other.numActions;
-		this->threads = new thread[numThreads];
-		this->initialInputs = new float[numNodes];
-		this->weights = new float[numWeights];
-		this->biases = new float[numNodes];
-		this->inputs = new float[numNodes];
-		this->outputs = new float[numNodes];
-		this->perceptions = new float[numPerseptions];
-		this->actions = new float[numActions];
-		memcpy(this->initialInputs, other.initialInputs, numNodes * sizeof(float));
-		memcpy(this->weights, other.weights, numWeights * sizeof(float));
-		memcpy(this->biases, other.biases, numNodes * sizeof(float));
-		memcpy(this->inputs, other.inputs, numNodes * sizeof(float));
-		memcpy(this->outputs, other.outputs, numNodes * sizeof(float));
-		memcpy(this->perceptions, other.perceptions, numPerseptions * sizeof(float));
-		memcpy(this->actions, other.actions, numActions * sizeof(float));
+		numNodes = other.numNodes;
+		numPerseptions = other.numPerseptions;
+		numActions = other.numActions;
+		numInputs = other.numInputs;
+		numOutputs = other.numOutputs;
+		numWeights = other.numWeights;
+		inputs = new float[numInputs];
+		outputs = new float[numOutputs];
+		initialInputs = new float[numNodes];
+		weights = new float[numWeights];
+		biases = new float[numOutputs];
+		numThreads = other.numThreads;
+		numNodesPerThread = other.numNodesPerThread;
+		numRemainingNodes = other.numRemainingNodes;
+		threads = new thread[numThreads];
+		memcpy(inputs, other.inputs, numInputs * sizeof(float));
+		memcpy(outputs, other.outputs, numOutputs * sizeof(float));
+		memcpy(initialInputs, other.initialInputs, numNodes * sizeof(float));
+		memcpy(weights, other.weights, numWeights * sizeof(float));
+		memcpy(biases, other.biases, numOutputs * sizeof(float));
 	}
 
 	Network& operator=(const Network& other) {
 		if (this != &other) {
-			this->numThreads = other.numThreads;
-			this->numNodesPerThread = other.numNodesPerThread;
-			this->numRemainingNodes = other.numRemainingNodes;
-			this->numNodes = other.numNodes;
-			this->numWeights = other.numWeights;
-			this->numPerseptions = other.numPerseptions;
-			this->numActions = other.numActions;
-			delete[] this->threads;
-			delete[] this->initialInputs;
-			delete[] this->weights;
-			delete[] this->biases;
-			delete[] this->inputs;
-			delete[] this->outputs;
-			delete[] this->perceptions;
-			delete[] this->actions;
-			this->threads = new thread[numThreads];
-			this->initialInputs = new float[numNodes];
-			this->weights = new float[numWeights];
-			this->biases = new float[numNodes];
-			this->inputs = new float[numNodes];
-			this->outputs = new float[numNodes];
-			this->perceptions = new float[numPerseptions];
-			this->actions = new float[numActions];
-			memcpy(this->initialInputs, other.initialInputs, numNodes * sizeof(float));
-			memcpy(this->weights, other.weights, numWeights * sizeof(float));
-			memcpy(this->biases, other.biases, numNodes * sizeof(float));
-			memcpy(this->inputs, other.inputs, numNodes * sizeof(float));
-			memcpy(this->outputs, other.outputs, numNodes * sizeof(float));
-			memcpy(this->perceptions, other.perceptions, numPerseptions * sizeof(float));
-			memcpy(this->actions, other.actions, numActions * sizeof(float));
+			numNodes = other.numNodes;
+			numPerseptions = other.numPerseptions;
+			numActions = other.numActions;
+			numInputs = other.numInputs;
+			numOutputs = other.numOutputs;
+			numWeights = other.numWeights;
+			delete[] inputs;
+			delete[] outputs;
+			delete[] initialInputs;
+			delete[] weights;
+			delete[] biases;
+			inputs = new float[numInputs];
+			outputs = new float[numOutputs];
+			initialInputs = new float[numNodes];
+			weights = new float[numWeights];
+			biases = new float[numOutputs];
+			numThreads = other.numThreads;
+			numNodesPerThread = other.numNodesPerThread;
+			numRemainingNodes = other.numRemainingNodes;
+			delete[] threads;
+			threads = new thread[numThreads];
+			memcpy(inputs, other.inputs, numInputs * sizeof(float));
+			memcpy(outputs, other.outputs, numOutputs * sizeof(float));
+			memcpy(initialInputs, other.initialInputs, numNodes * sizeof(float));
+			memcpy(weights, other.weights, numWeights * sizeof(float));
+			memcpy(biases, other.biases, numOutputs * sizeof(float));
 		}
 		return *this;
 	}
 
 	Network(Network&& other) noexcept {
-		this->numThreads = other.numThreads;
-		this->numNodesPerThread = other.numNodesPerThread;
-		this->numRemainingNodes = other.numRemainingNodes;
-		this->numNodes = other.numNodes;
-		this->numWeights = other.numWeights;
-		this->numPerseptions = other.numPerseptions;
-		this->numActions = other.numActions;
-		this->threads = other.threads;
-		this->initialInputs = other.initialInputs;
-		this->weights = other.weights;
-		this->biases = other.biases;
-		this->inputs = other.inputs;
-		this->outputs = other.outputs;
-		this->perceptions = other.perceptions;
-		this->actions = other.actions;
-		other.threads = nullptr;
+		numNodes = other.numNodes;
+		numPerseptions = other.numPerseptions;
+		numActions = other.numActions;
+		numInputs = other.numInputs;
+		numOutputs = other.numOutputs;
+		numWeights = other.numWeights;
+		inputs = other.inputs;
+		outputs = other.outputs;
+		initialInputs = other.initialInputs;
+		weights = other.weights;
+		biases = other.biases;
+		numThreads = other.numThreads;
+		numNodesPerThread = other.numNodesPerThread;
+		numRemainingNodes = other.numRemainingNodes;
+		threads = other.threads;
+		other.inputs = nullptr;
+		other.outputs = nullptr;
 		other.initialInputs = nullptr;
 		other.weights = nullptr;
 		other.biases = nullptr;
-		other.inputs = nullptr;
-		other.outputs = nullptr;
-		other.perceptions = nullptr;
-		other.actions = nullptr;
+		other.threads = nullptr;
 	}
 
 	Network& operator=(Network&& other) noexcept {
 		if (this != &other) {
-			this->numThreads = other.numThreads;
-			this->numNodesPerThread = other.numNodesPerThread;
-			this->numRemainingNodes = other.numRemainingNodes;
-			this->numNodes = other.numNodes;
-			this->numWeights = other.numWeights;
-			this->numPerseptions = other.numPerseptions;
-			this->numActions = other.numActions;
-			delete[] this->threads;
-			delete[] this->initialInputs;
-			delete[] this->weights;
-			delete[] this->biases;
-			delete[] this->inputs;
-			delete[] this->outputs;
-			delete[] this->perceptions;
-			delete[] this->actions;
-			this->threads = other.threads;
-			this->initialInputs = other.initialInputs;
-			this->weights = other.weights;
-			this->biases = other.biases;
-			this->inputs = other.inputs;
-			this->outputs = other.outputs;
-			this->perceptions = other.perceptions;
-			this->actions = other.actions;
-			other.threads = nullptr;
+			numNodes = other.numNodes;
+			numPerseptions = other.numPerseptions;
+			numActions = other.numActions;
+			numInputs = other.numInputs;
+			numOutputs = other.numOutputs;
+			numWeights = other.numWeights;
+			delete[] inputs;
+			delete[] outputs;
+			delete[] initialInputs;
+			delete[] weights;
+			delete[] biases;
+			delete[] threads;
+			inputs = other.inputs;
+			outputs = other.outputs;
+			initialInputs = other.initialInputs;
+			weights = other.weights;
+			biases = other.biases;
+			numThreads = other.numThreads;
+			numNodesPerThread = other.numNodesPerThread;
+			numRemainingNodes = other.numRemainingNodes;
+			threads = other.threads;
+			other.inputs = nullptr;
+			other.outputs = nullptr;
 			other.initialInputs = nullptr;
 			other.weights = nullptr;
 			other.biases = nullptr;
-			other.inputs = nullptr;
-			other.outputs = nullptr;
-			other.perceptions = nullptr;
-			other.actions = nullptr;
+			other.threads = nullptr;
 		}
 		return *this;
 	}
 
 	void Initialize() {
 		auto start = std::chrono::high_resolution_clock::now();
-		uint64_t row = 0;
+		uint64_t currentOutput = 0;
 		for (uint64_t i = 0; i < numRemainingNodes; i++) {
-			threads[i] = thread(initParams, initialInputs, weights, biases, numNodes, row, numNodesPerThread + 1);
-			row += numNodesPerThread + 1;
+			threads[i] = thread(initParams, initialInputs, weights, biases, numNodes, numInputs, currentOutput, numNodesPerThread + 1);
+			currentOutput += numNodesPerThread + 1;
 		}
 		for (uint64_t i = numRemainingNodes; i < numThreads; i++) {
-			threads[i] = thread(initParams, initialInputs, weights, biases, numNodes, row, numNodesPerThread);
-			row += numNodesPerThread;
+			threads[i] = thread(initParams, initialInputs, weights, biases, numNodes, numInputs, currentOutput, numNodesPerThread);
+			currentOutput += numNodesPerThread;
 		}
 		for (int i = 0; i < numThreads; i++) {
 			threads[i].join();
@@ -218,69 +208,72 @@ public:
 		cout << "Elapsed time: " << elapsed.count() / 10 << " s" << endl;
 	}
 
-	void FeedForward() {
-		auto start = std::chrono::high_resolution_clock::now();
-		for (int itr = 0; itr < 10; itr++) {	// must be even for pointers to be back, or make param for itr and use if or smth
-			uint64_t row = 0;
-			for (uint64_t i = 0; i < numRemainingNodes; i++) {
-				threads[i] = thread(multiplyWeightsAndBiases, weights, biases, inputs, outputs, perceptions, actions, numNodes, row, numNodesPerThread + 1);
-				row += numNodesPerThread + 1;
-			}
-			for (uint64_t i = numRemainingNodes; i < numThreads; i++) {
-				threads[i] = thread(multiplyWeightsAndBiases, weights, biases, inputs, outputs, perceptions, actions, numNodes, row, numNodesPerThread);
-				row += numNodesPerThread;
-			}
-			for (int i = 0; i < numThreads; i++) {
-				threads[i].join();
-			}
-			swap(inputs, outputs);
-		}
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> elapsed = end - start;
-		cout << "Elapsed time: " << elapsed.count() / 10 << " s" << endl;
-	}
+	//void FeedForward() {
+	//	auto start = std::chrono::high_resolution_clock::now();
+	//	for (int itr = 0; itr < 10; itr++) {	// must be even for pointers to be back, or make param for itr and use if or smth
+	//		uint64_t currentOutput = 0;
+	//		for (uint64_t i = 0; i < numRemainingNodes; i++) {
+	//			threads[i] = thread(multiplyWeightsAndBiases, weights, biases, inputs, outputs, perceptions, actions, numNodes, currentOutput, numNodesPerThread + 1);
+	//			currentOutput += numNodesPerThread + 1;
+	//		}
+	//		for (uint64_t i = numRemainingNodes; i < numThreads; i++) {
+	//			threads[i] = thread(multiplyWeightsAndBiases, weights, biases, inputs, outputs, perceptions, actions, numNodes, currentOutput, numNodesPerThread);
+	//			currentOutput += numNodesPerThread;
+	//		}
+	//		for (int i = 0; i < numThreads; i++) {
+	//			threads[i].join();
+	//		}
+	//		swap(inputs, outputs);
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	std::chrono::duration<double> elapsed = end - start;
+	//	cout << "Elapsed time: " << elapsed.count() / 10 << " s" << endl;
+	//}
 
 	void PrintParams() {
-		for (uint64_t i = 0; i < numNodes; i++) {
-			cout << initialInputs[i] << " ";
-		}
-		cout << endl << endl;
-
-		for (uint64_t i = 0; i < numNodes; i++) {
-			for (uint64_t j = 0; j < numNodes; j++) {
-				cout << weights[i * numNodes + j] << " ";
+		cout << "Weights:\n";
+		for (uint64_t i = 0; i < numOutputs; i++) {
+			for (uint64_t j = 0; j < numInputs; j++) {
+				cout << weights[i * numInputs + j] << " ";
 			}
 			cout << endl;
 		}
 		cout << endl;
 
-		for (uint64_t i = 0; i < numNodes; i++) {
+		cout << "Biases:\n";
+		for (uint64_t i = 0; i < numOutputs; i++) {
 			cout << biases[i] << " ";
 		}
-		cout << endl << endl;
-	}
+		cout << endl;
 
-	void PrintOutputs() {
-		for (int i = 0; i < numNodes; i++) {
-			cout << outputs[i] << " ";
+		cout << "Initial Inputs:\n";
+		for (uint64_t i = 0; i < numNodes; i++) {
+			cout << initialInputs[i] << " ";
 		}
 		cout << endl;
 	}
 
-	void LinearInput() {	// for testing until initialInput is added
-		for (int i = 0; i < numNodes; i++) {
-			inputs[i] = i;
+	void PrintOutputs() {
+		cout << "Input:\n";
+		for (uint64_t i = 0; i < numInputs; i++) {
+			cout << inputs[i] << " ";
 		}
+		cout << endl;
+
+		cout << "Outputs:\n";
+		for (uint64_t i = 0; i < numOutputs; i++) {
+			cout << outputs[i] << " ";
+		}
+		cout << endl;
 	}
 };
 
 int main() {
-	Network net(10, 10);
+	Network net(10000, 2, 1, 10);
 	net.Initialize();
 	//net.PrintParams();
-	net.LinearInput();
-	net.FeedForward();
-	net.PrintOutputs();
+	//net.FeedForward();
+	//net.PrintOutputs();
 
 	return 0;
 }
